ea6fc98fff71ee2a64b60db3cafb9123bea08ea7
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-11-17 23:39:53 +0500

    Base Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-export default function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+
+const App = () => {
   return (
-    <h1 className="">
-      Hello world!
-    </h1>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        {/* You can add more routes here */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
